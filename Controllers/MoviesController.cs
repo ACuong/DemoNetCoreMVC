9fab22384b8d83fc020febba331182aa6499af2d
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-12-10 16:45:19 +0700

    UploadFile Excel Movies

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -6,21 +6,52 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DemoDotNetMVC.Models;
+using DemoDotNetMVC.Models.Process;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace DemoDotNetMVC.Controllers
 {
     public class MoviesController : Controller
     {
+        ExcelProcess _excelPro = new ExcelProcess();
         private readonly ApplicationDBContext _context;
 
         public MoviesController(ApplicationDBContext context)
         {
             _context = context;
         }
+        
+        public IConfiguration Configuration{get;}
+
+        private int WriteDatatableToDatabase(DataTable dt)
+        {
+        try{
+            var con = Configuration.GetConnectionString("ApplicationContext");
+            //using bulkcopy to copy data from datatable to databse
+            SqlBulkCopy bulkcopy = new SqlBulkCopy(con);
+            bulkcopy.DestinationTableName = "Movies";
+            bulkcopy.ColumnMappings.Add(0,"MoviesID");
+            bulkcopy.ColumnMappings.Add(1,"MoviesName");
+            bulkcopy.ColumnMappings.Add(3,"Price");
+            bulkcopy.ColumnMappings.Add(4,"Genre");
+            bulkcopy.ColumnMappings.Add(5,"Price");
+            bulkcopy.WriteToServer(dt);
+        }
+            catch{
+            return 0;	
+        }
+            return dt.Rows.Count;
+          
+        }
 
         // GET: Movies
        
-public async Task<IActionResult> Index(string movieGenre, string searchString)
+    public async Task<IActionResult> Index(string movieGenre, string searchString)
         {// Use LINQ to get list of genres.
             IQueryable<string> genreQuery = from m in _context.Movies
                                             orderby m.Genre
@@ -78,16 +109,50 @@ public async Task<IActionResult> Index(string movieGenre, string searchString)
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MoviesID,MoviesName,Price,Genre,Rating")] Movies movies)
+        public async Task<IActionResult> Create([Bind("MoviesID,MoviesName,Price,Genre,Rating")] Movies movies, IFormFile file)
         {
-            if (ModelState.IsValid)
+            if (file != null)
             {
-                _context.Add(movies);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+            
+                else
+                {
+                    //rename file when upload to server
+                    //tao duong dan /Uploads/Excels de luu file upload len server
+                    var fileName = "DanhSach";
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName + fileExtension);
+                    var fileLocation = new FileInfo(filePath).ToString();
+
+                    if (ModelState.IsValid)
+                    {
+                        //upload file to server
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                //save file to server
+                                await file.CopyToAsync(stream);
+                                //read data from file and write to database
+                                //_excelPro la doi tuong xu ly file excel ExcelProcess
+                                var dt = _excelPro.ExcelToDataTable(fileLocation);
+                                //ghi du lieu datatable vao database
+                                WriteDatatableToDatabase(dt);
+                                
+                            }
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
             }
+            
             return View(movies);
         }
+
+         
 
         // GET: Movies/Edit/5
         public async Task<IActionResult> Edit(string id)
